b20768008b64a23d2591ac6f395e0503594401a5
yalmazhasanbutt <yalmazhasanbutt@gmail.com>
2018-10-25 14:04:04 +0500

    initial commit

--- SplashActivity.java
+++ SplashActivity.java
@@ -28,11 +28,9 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-
                 Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
-
             }
         }, SPLASH_DISPLAY_LENGTH);
 
